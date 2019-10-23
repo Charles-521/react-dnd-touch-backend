f2be22727955c9930ad9454a1407f4e324601216
Charles Xu <c.xu@compass-digital.com>
2019-10-23 17:26:05 +0800

    last commit

--- examples/js/DraggableItem.jsx
+++ examples/js/DraggableItem.jsx
@@ -15,88 +15,88 @@ import classnames from 'classnames';
  * react-dnd's docs: http://gaearon.github.io/react-dnd/docs-overview.html
  */
 const dragSource = {
-    beginDrag (props) {
-        return {
-            id: props.id,
-            listId: props.listId,
-            name: props.name,
-            onReorder: props.onReorder
-        };
-    }
+  beginDrag(props) {
+    return {
+      id: props.id,
+      listId: props.listId,
+      name: props.name,
+      onReorder: props.onReorder
+    };
+  }
 };
 
 const dropTarget = {
-    drop (props, monitor) {
-        const item = monitor.getItem();
-        // Don't trigger reorder if it's to the same spot
-        if (
-            item.listId === props.listId &&
-            item.id === props.id
-        ) {
-            return;
-        }
-        item.onReorder(
-            {
-                listId: item.listId,
-                id: item.id
-            },
-            {
-                listId: props.listId,
-                id: props.id
-            });
+  drop(props, monitor) {
+    const item = monitor.getItem();
+    // Don't trigger reorder if it's to the same spot
+    if (
+      item.listId === props.listId &&
+      item.id === props.id
+    ) {
+      return;
     }
+    item.onReorder({
+      listId: item.listId,
+      id: item.id
+    }, {
+      listId: props.listId,
+      id: props.id
+    });
+  }
 };
 
 class Item extends React.Component {
-    render () {
-        const className = classnames('item', {
-            'is-over': this.props.isOver
-        });
+  render() {
+    const className = classnames('item', {
+      'is-over': this.props.isOver
+    });
 
-        let content = (
-            <li className={className}>
-                {this.props.id} <a href="http://google.com">{this.props.name}</a>
+    let content = (
+      <li className={className}>
+              {this.props.id} <a href="http://google.com">{this.props.name}</a>
             </li>
-        );
+    );
 
-        // Connect as drag source
-        content = this.props.connectDragSource(content, { dropEffect: 'move' });
-        // Connect as drop target
-        content = this.props.connectDropTarget(content);
-        // Connect to drag layer
-        content = this.props.connectDragPreview(content);
+    // Connect as drag source
+    content = this.props.connectDragSource(content, {
+      dropEffect: 'move'
+    });
+    // Connect as drop target
+    content = this.props.connectDropTarget(content);
+    // Connect to drag layer
+    content = this.props.connectDragPreview(content);
 
-        return content;
-    }
+    return content;
+  }
 }
 
 Item.PropTypes = {
-    id: React.PropTypes.number.isRequired,
-    listId: React.PropTypes.number.isRequired,
-    text: React.PropTypes.string,
+  id: React.PropTypes.number.isRequired,
+  listId: React.PropTypes.number.isRequired,
+  text: React.PropTypes.string,
 
-    // react-dnd props
-    connectDragSource: React.PropTypes.func,
-    connectDropTarget: React.PropTypes.func,
-    connectDragPreview: React.PropTypes.func,
-    onReorder: React.PropTypes.func,
-    isDragging: React.PropTypes.bool,
-    isOver: React.PropTypes.bool
+  // react-dnd props
+  connectDragSource: React.PropTypes.func,
+  connectDropTarget: React.PropTypes.func,
+  connectDragPreview: React.PropTypes.func,
+  onReorder: React.PropTypes.func,
+  isDragging: React.PropTypes.bool,
+  isOver: React.PropTypes.bool
 };
 
 export default DragSource(
-    'Item',
-    dragSource,
-    (connect, monitor) => ({
-        connectDragSource: connect.dragSource(),
-        connectDragPreview: connect.dragPreview(),
-        isDragging: monitor.isDragging()
-    })
+  'Item',
+  dragSource,
+  (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  })
 )(DropTarget(
-    'Item',
-    dropTarget,
-    (connect, monitor) => ({
-        connectDropTarget: connect.dropTarget(),
-        isOver: monitor.isOver()
-    })
+  'Item',
+  dropTarget,
+  (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  })
 )(Item));
--- examples/js/ItemPreview.jsx
+++ examples/js/ItemPreview.jsx
@@ -7,63 +7,68 @@
 import React from 'react';
 import DragLayer from 'react-dnd/lib/DragLayer';
 
-function collect (monitor) {
-    var item = monitor.getItem();
-    return {
-        id: item && item.id,
-        name: item && item.name,
-        currentOffset: monitor.getSourceClientOffset(),
-        isDragging: monitor.isDragging()
-    };
+function collect(monitor) {
+  var item = monitor.getItem();
+  return {
+    id: item && item.id,
+    name: item && item.name,
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging()
+  };
 }
 
-function getItemStyles (currentOffset) {
-    if (!currentOffset) {
-        return {
-            display: 'none'
-        };
-    }
-
-    // http://www.paulirish.com/2012/why-moving-elements-with-translate-is-better-than-posabs-topleft/
-    var x = currentOffset.x;
-    var y = currentOffset.y;
-    var transform = `translate(${x}px, ${y}px)`;
-
+function getItemStyles(currentOffset) {
+  if (!currentOffset) {
     return {
-        pointerEvents: 'none',
-        transform: transform,
-        WebkitTransform: transform
+      display: 'none'
     };
+  }
+
+  // http://www.paulirish.com/2012/why-moving-elements-with-translate-is-better-than-posabs-topleft/
+  var x = currentOffset.x;
+  var y = currentOffset.y;
+  var transform = `translate(${x}px, ${y}px)`;
+
+  return {
+    opacity: '0.8',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    pointerEvents: 'none',
+    transform: transform,
+    WebkitTransform: transform
+  };
 }
 
-function ItemPreview ({
-    id,
-    name,
-    isDragging,
-    currentOffset
+function ItemPreview({
+  id,
+  name,
+  isDragging,
+  currentOffset
 }) {
-    if (!isDragging) {
-        return <li></li>;
-    }
+  if (!isDragging) {
+    return <li>Preview</li>;
+  }
 
-    return (
-        <div
-            className="item preview"
-            style={getItemStyles(currentOffset)}
-        >
-            {id} {name}
-        </div>
-    );
+  return (
+    <div
+      className="item preview"
+      style={getItemStyles(currentOffset)}
+    >
+      {id} {name} preview
+    </div>
+  );
 }
 
 ItemPreview.propTypes = {
-    id: React.PropTypes.string,
-    name: React.PropTypes.string,
-    currentOffset: React.PropTypes.shape({
-        x: React.PropTypes.number,
-        y: React.PropTypes.number
-    }),
-    isDragging: React.PropTypes.bool
+  id: React.PropTypes.string,
+  name: React.PropTypes.string,
+  currentOffset: React.PropTypes.shape({
+    x: React.PropTypes.number,
+    y: React.PropTypes.number
+  }),
+  isDragging: React.PropTypes.bool
 };
 
 export default DragLayer(collect)(ItemPreview);
